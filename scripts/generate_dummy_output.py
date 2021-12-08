9246053eea72d1469dd820a40a14eb4051ff1b60
CADE LILLEY <cadelilley@gmail.com>
2021-12-07 16:52:54 -0800

    Reformatted directories, added dummy gen script

---
+++ scripts/generate_dummy_output.py
@@ -0,0 +1,23 @@
+import pickle as pkl
+import sys
+
+# python generate_dummy_output.py {step_count} {particle_count}
+
+def create_dummy_pkl(step_count=100, particle_count=10):
+    # Creates matrix of evenly spaced particle [x, y, z] coordinates
+    steps = []
+    for step in range(step_count):
+        steps.append([])
+        for particle in range(particle_count):
+            steps[step].append([particle, step, 0]) # [x, y, z]
+    file = open(f"steps_{step_count}_particles_{particle_count}.pkl", 'wb')
+    pkl.dump(steps, file)
+
+def main():
+    if (len(sys.argv) > 2):
+        create_dummy_pkl(int(sys.argv[1]), int(sys.argv[2]))
+    else:
+        create_dummy_pkl()
+
+if __name__ == "__main__":
+    main()
