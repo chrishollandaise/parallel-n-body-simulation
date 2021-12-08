c7a19c07f2d25d6a731243698499135fccc51045
Chris Holland <chrishollandaise@gmail.com>
2021-12-08 09:01:19 -0800

    Finished GUI demo, made modifications to dummy script, added imgs

--- scripts/generate_dummy_output.py
+++ scripts/generate_dummy_output.py
@@ -1,20 +1,26 @@
 import pickle as pkl
 import sys
+from os import path, mkdir
 
 # python generate_dummy_output.py {epochs} {particle_count}
 
-# 500x500 canvas
-SIZE = 500
+# X_SIZE X Y_SIZE canvas
+X_SIZE = 720
+Y_SIZE = 480
+
+# In the GUI, particles have a radius that is greater than 1
+PARTICLE_RADIUS = 40
 
 # Pickle file output
+out_dir = "profiles/"
 
 # steps contains each step as array
 # each step contains each particle
 # each particle contains [x, y, z] elements
 
 def create_dummy_pkl(step_count=100, particle_count=10):
-    y_stride = SIZE/(step_count-1)
-    x_stride = SIZE/(particle_count-1)
+    y_stride = Y_SIZE/(step_count-1)
+    x_stride = X_SIZE/(particle_count-1) + PARTICLE_RADIUS
     
     # Creates matrix of evenly spaced particle [x, y, z] coordinates
     steps = []
@@ -23,7 +29,11 @@ def create_dummy_pkl(step_count=100, particle_count=10):
         for particle in range(particle_count):
             steps[step].append([particle*x_stride, step*y_stride, 0.0]) # [x, y, z]
         print(steps[step])
-    file = open(f"steps_{step_count}_particles_{particle_count}.pkl", 'wb')
+    
+    if not path.exists(out_dir):
+        mkdir(out_dir)
+
+    file = open(f"{out_dir}steps_{step_count}_particles_{particle_count}.pkl", 'wb')
     pkl.dump(steps, file)
 
 def main():
--- scripts/gui.py
+++ scripts/gui.py
@@ -1,22 +1,50 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics import Rectangle, Color
-
-class MainBoxLayout(BoxLayout):
-    pass
+from kivy.graphics import Ellipse
+from kivy.config import Config
+import pickle
+import os
+from kivy.clock import Clock
 
 class MainCanvas(Widget):
+        
     def __init__(self, **kwargs):
         super(MainCanvas, self).__init__(**kwargs)
+        self.load()
+        # used to iterate through the epochs
+        self.inc = 0
 
+        with self.canvas.before:
+            # initialize the ellipses to their first positions
+            self.particles = [Ellipse(size=(40, 40),pos=(nbody[0], nbody[1])) for nbody in self.epochs[0]]
+ 
         with self.canvas:
-            Color(1, 0, 0, 1)
-            self.rect = Rectangle(pos=(0, 0), size=(20, 20))
+            Clock.schedule_interval(self.update, 1.0/60.0)
+
+    def update(self, dt):
+        iter = self.inc % len(self.epochs)
+
+        # update the positions of the ellipses
+        for idx, particle in enumerate(self.particles):
+            particle.pos = (self.epochs[iter][idx][0], self.epochs[iter][idx][1])
+        
+        # next epoch
+        self.inc += 1    
+
+    def load(self):
+        self.epochs = pickle.load(open(os.path.join(os.getcwd(), 'profiles/steps_600_particles_30.pkl'), 'rb')) 
+        
 
 class MainApp(App):
     def build(self):
+        Config.set('graphics', 'resizable', False)
+        Config.set('graphics', 'width', '720')
+        Config.set('graphics', 'height', '480')
         self.title = 'N-Body Simulator'
 
-main = MainApp()
-main.run()
+def main():
+    app = MainApp()
+    app.run()
+
+if __name__ == '__main__':
+    main()
