f51c974e31f06b32d9fc0f485ad274ffc85521b4
CADE LILLEY <cadelilley@gmail.com>
2021-12-10 14:19:08 -0800

    Fixed parameter passing logical error

--- scripts/nbody_cpu.py
+++ scripts/nbody_cpu.py
@@ -3,39 +3,34 @@ import pickle as pkl
 from math import sqrt
 
 G = 6.67 * 10 ** -11
-TIME_STEP = 2500
-STEPS = 1000
+TIME_STEP = 100
+STEPS = 60000
 # Force of gravity = (G * M * m) / d^2
 # G = 6.67 * 10^-11 newton square meter kg^-2
 # M is mass of colleague
 # m is current mass
-g_force = lambda p1, p2: ( G * p2.mass * p1.mass ) / (dist( p1, p2 ) ** 2)
+g_force = lambda p1, p2: ( ( p2.mass * p1.mass ) / (dist( p1, p2 ) ** 2) ) * G
 dist = lambda p1, p2: sqrt( ( p1.x - p2.x ) ** 2 + ( p1.y - p2.y ) ** 2 + ( p1.z - p2.z ) ** 2 )
-f_vector = lambda total_f, vector_d, total_d: total_f * ( ( -vector_d ) / total_d )
-# Return acceleration given force and mass
-# Used on a per vector basis
-a = lambda f, m: f / m
 
 '''
-def f_vector(c_p1, c_p2, total_f, total_d):
-    f = abs(total_f * ( ( c_p2 - c_p1 ) / total_d ))
-    print(f"{-f}\n" if c_p2-c_p1 > 0 else f"{f}\n" if f != 0 else "",end="")
-    return f if c_p2-c_p1 > 0 else -f
-'''
-
-'''
-Returns final velocity of p1 after being effected by p2
+Updates p1 final velocity after being affected by p2
 '''
 def update_velocity(p1, p2):
-    # Net force
+    # Net force in 3D
     f_net = g_force( p1 , p2 )
+
     # Total distance in 3 planes
     d = dist( p1, p2 )
 
-    f_x, f_y, f_z = f_vector( p1.x - p2.x , f_net , d ) , f_vector( p1.y - p2.y , f_net , d ) , f_vector( p1.z - p2.z , f_net , d )
-    p1.v_x += a(f_x, p1.mass) * TIME_STEP
-    p1.v_y += a(f_y, p1.mass) * TIME_STEP
-    p1.v_z += a(f_z, p1.mass) * TIME_STEP
+    # Compute each force vector
+    f_x = f_net * ( ( p2.x - p1.x ) / d )
+    f_y = f_net * ( ( p2.y - p1.y ) / d )
+    f_z = f_net * ( ( p2.z - p1.z ) / d )
+
+    # Update each velocity with the acceleration multiplied by the time_step
+    p1.v_x += ( f_x / p1.mass ) * TIME_STEP
+    p1.v_y += ( f_y / p1.mass ) * TIME_STEP
+    p1.v_z += ( f_z / p1.mass ) * TIME_STEP
 
 def update_pos(p):
     p.x += p.v_x * TIME_STEP
@@ -51,7 +46,7 @@ def run_simulation(epochs, particles):
                     update_velocity( p1 , p2 )
         for p in particles:
             update_pos(p)
-            epochs[epoch].append([p.x, p.y, p.z])
+            epochs[epoch].append( [ p.x , p.y , p.z ] )
 
 def output_results(epochs):
     file = open(f"{STEPS}steps_{len(particles)}_particles_{TIME_STEP}spe.pkl", 'wb')
@@ -63,18 +58,18 @@ if __name__ == "__main__":
     #particles = [ nbody.Particle( 10000 , 250 , 250 , 0 ), nbody.Particle( 1000000 , -125 , -125 , 0 )]
     import random as rand
     # Generate 100 random particles between x=0 to 500
-    particles = []
-    for i in range(1_000_000):
-        particles.append(nbody.Particle(x=rand.randint(0, 500), y=rand.randint(0, 500), z=0, mass=rand.randint(10, 500000)))
+    #particles = []
+    #for i in range(1_000_000):
+    #    particles.append(nbody.Particle(x=rand.randint(0, 500), y=rand.randint(0, 500), z=0, mass=rand.randint(10, 500000)))
 
-    # particles = [ nbody.Particle( 10000 , 250 , 250 , 0 ), nbody.Particle( 1000000 , -125 , -125 , 0 )]
+    particles = [ nbody.Particle( 1000000 , -25 , -0 , 0 ), nbody.Particle( 1000000 , 25 , 0 , 0 )]
     epochs = []
-    run_simulation(epochs, particles)
-    file = open(f"profiles/{STEPS}steps_{len(particles)}_particles_{TIME_STEP}spe.pkl", 'wb')
+    #run_simulation(epochs, particles)
+    #file = open(f"profiles/{STEPS}steps_{len(particles)}_particles_{TIME_STEP}spe.pkl", 'wb')
     # particles = [nbody.Particle( 120 , -100 , -100 , 0, xvel=0.0001, yvel=0.0001), nbody.Particle( 120 , 100 , 100 , 0 , xvel=-0.0001, yvel=-0.0001)]
     epochs = []
     run_simulation(epochs, particles)
-    output_results(epochs)
+    #output_results(epochs)
     for d in epochs:
         print(d)
     for p in particles:
